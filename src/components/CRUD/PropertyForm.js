c335f7bb3b8dff19d4e2a3c2000fb52a629f387c
Tim Park <timpark0807@gmail.com>
2020-04-13 18:35:05 -0700

    Refactor Property Form from class into functional component

--- src/components/CRUD/PropertyForm.js
+++ src/components/CRUD/PropertyForm.js
@@ -4,28 +4,23 @@ import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField';
 import Cookies from 'js-cookie'
 
-class PropertyForm extends Component { 
+function PropertyForm() { 
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            address: '',
-            zipcode: '',
-            price: '',
-            category:''
+    const [fields, setFields] = React.useState({address: '',
+                                                zipcode: '',
+                                                price: '',
+                                                category:''
+                                            })
+
+    const changeHandler = (event) => {
+            setFields({
+                ...fields,
+                [event.target.name] : event.target.value
+            })
         }
-    }  
 
-    changeHandler = (event) => {
-        this.setState({
-            [event.target.name] : event.target.value
-        })
-    }
-
-    handleSubmit = event => {
-        console.log(Cookies.get("token"))
-        console.log(Cookies.get("token"))
-        Axios.post("http://localhost:8000/api/property",  this.state, {headers: {"Authorization":"Bearer " + Cookies.get("token")}})
+    const handleSubmit = event => {
+        Axios.post("http://localhost:8000/api/property",  fields, {headers: {"Authorization":"Bearer " + Cookies.get("token")}})
         .then(response => {
             console.log(response)
         })
@@ -34,35 +29,92 @@ class PropertyForm extends Component {
         })
     }
 
-
-    render () {
-        const { address, zipcode, price, category } = this.state
-
-        return (
-            <form onSubmit={this.handleSubmit}>
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
                 <div>
-                    <div>
-                    <TextField id="standard-basic" label="Address" name="address" value={address} onChange={this.changeHandler}></TextField>
-                    </div>
-
-                    <div>
-                    <TextField id="standard-basic" label="Zipcode" name="zipcode" value={zipcode} onChange={this.changeHandler}></TextField>
-                    </div>
-
-                    <div>
-                    <TextField id="standard-basic" label="Purchase Price" name="price" value={price} onChange={this.changeHandler}></TextField>
-                    </div>
-                    <div>
-                    <TextField id="standard-basic" label="Category" name="category" value={category} onChange={this.changeHandler}></TextField>
-                    </div>
-       
+                <TextField id="standard-basic" label="Address" name="address" value={fields.address} onChange={changeHandler}></TextField>
                 </div>
-                <Button variant="contained" color="primary" type="submit" style={{"margin-top":20}}>
-                Submit
-                </Button>
-            </form>
-        )
-    }
+
+                <div>
+                <TextField id="standard-basic" label="Zipcode" name="zipcode" value={fields.zipcode} onChange={changeHandler}></TextField>
+                </div>
+
+                <div>
+                <TextField id="standard-basic" label="Purchase Price" name="price" value={fields.price} onChange={changeHandler}></TextField>
+                </div>
+                <div>
+                <TextField id="standard-basic" label="Category" name="category" value={fields.category} onChange={changeHandler}></TextField>
+                </div>
+    
+            </div>
+            <Button variant="contained" color="primary" type="submit" style={{"margin-top":20}}>
+            Submit
+            </Button>
+        </form>
+    )
+    
 }
+
+// class PropertyForm extends Component { 
+
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             address: '',
+//             zipcode: '',
+//             price: '',
+//             category:''
+//         }
+//     }  
+
+//     changeHandler = (event) => {
+//         this.setState({
+//             [event.target.name] : event.target.value
+//         })
+//     }
+
+//     handleSubmit = event => {
+//         console.log(Cookies.get("token"))
+//         console.log(Cookies.get("token"))
+//         Axios.post("http://localhost:8000/api/property",  this.state, {headers: {"Authorization":"Bearer " + Cookies.get("token")}})
+//         .then(response => {
+//             console.log(response)
+//         })
+//         .catch(error =>{
+//             console.log(error)
+//         })
+//     }
+
+
+//     render () {
+//         const { address, zipcode, price, category } = this.state
+
+//         return (
+//             <form onSubmit={this.handleSubmit}>
+//                 <div>
+//                     <div>
+//                     <TextField id="standard-basic" label="Address" name="address" value={address} onChange={this.changeHandler}></TextField>
+//                     </div>
+
+//                     <div>
+//                     <TextField id="standard-basic" label="Zipcode" name="zipcode" value={zipcode} onChange={this.changeHandler}></TextField>
+//                     </div>
+
+//                     <div>
+//                     <TextField id="standard-basic" label="Purchase Price" name="price" value={price} onChange={this.changeHandler}></TextField>
+//                     </div>
+//                     <div>
+//                     <TextField id="standard-basic" label="Category" name="category" value={category} onChange={this.changeHandler}></TextField>
+//                     </div>
+       
+//                 </div>
+//                 <Button variant="contained" color="primary" type="submit" style={{"margin-top":20}}>
+//                 Submit
+//                 </Button>
+//             </form>
+//         )
+//     }
+// }
 
 export default PropertyForm 
