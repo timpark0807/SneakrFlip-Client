927a9398bfcfc3b8a9a6a1da1ebfb07618616570
Tim Park <timpark0807@gmail.com>
2020-04-13 22:12:06 -0700

    Confirm Delete dialog formatting

--- src/components/CRUD/ConfirmDelete.js
+++ src/components/CRUD/ConfirmDelete.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import axios from 'axios'
@@ -21,30 +23,37 @@ function ConfirmDelete({open, handleClose, post_id}) {
     }
 
     return (
-    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" align="center" maxWidth="sm" fullWidth="true">
+    <Dialog 
+        open={open} 
+        onClose={handleClose} 
+        aria-labelledby="form-dialog-title"
+        aria-describedby="alert-dialog-description" 
+        maxWidth="sm" 
+        fullWidth="true"
+    >
     <DialogTitle id="alert-dialog-title">
-                  Are you sure you want to delete?
+                  {"Are you sure you want to delete?"}
     </DialogTitle>
     <Divider light />
-
+    <DialogContent>
+    <DialogContentText id="alert-dialog-description">
+            This action will permanently delete this property entry from your records. 
+    </DialogContentText>
+    </DialogContent>
     <DialogActions>
     <Button
-          variant="contained"
           onClick={() => handleClose(false)}
           color="default">
-            No
+            Cancel
     </Button>
     <Button
-          variant="contained"
           onClick={() => {handleClose(false)
                         handleDelete(post_id)
                     }
                 }
           color="primary">
-            Yes
+            Confirm
     </Button>
-
-
     </DialogActions>
   </Dialog>
     )
