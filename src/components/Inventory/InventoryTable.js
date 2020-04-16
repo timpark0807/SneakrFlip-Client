64e1c2fc7eb7e08d5ae9e6ed0a89c25327110213
Tim Park <timpark0807@gmail.com>
2020-04-15 21:30:17 -0700

    Feature request to mark items as sold

--- src/components/Inventory/InventoryTable.js
+++ src/components/Inventory/InventoryTable.js
@@ -10,25 +10,17 @@ import TableRow from '@material-ui/core/TableRow';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
-import ConfirmDelete from './../CRUD/ConfirmDelete.js'
 import InventoryDelete from './InventoryDelete.js'
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 
 function InventoryTable() {
     
     const [posts, setPosts] = React.useState([])
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false)
     const [deleteID, setDeleteID] = React.useState('')
 
     // load database entries when page loads
-    useEffect(() => { axios.get('http://localhost:8000/api/item', {headers: {"Authorization":"Bearer " + Cookies.get("token")}})
-                    .then(response =>{
-                        setPosts(response.data)
-                    })
-                    .catch(error=>{
-                        console.log(error)
-                    }
-                )
-                }, [])
+    useEffect(() => listItems(), [])
 
     const handleClickOpen = (post_id) => {
       setOpen(true);
@@ -38,7 +30,31 @@ function InventoryTable() {
     const handleClose = () => {
       setOpen(false);
     };
-  
+    
+    const listItems = () => {axios.get('http://localhost:8000/api/item', {headers: {"Authorization":"Bearer " + Cookies.get("token")}})
+                            .then(response =>{
+                                setPosts(response.data)
+                            })
+                            .catch(error=>{
+                                console.log(error)
+                            })
+                        }
+
+    const handleSoldClick = (item_id) => {
+        deleteRequest(item_id)
+    }
+    
+    const deleteRequest = (item_id) => {
+        axios.post("http://localhost:8000/api/item/updatestatus", {"_id": item_id}, {headers: {"Authorization":"Bearer " + Cookies.get("token")}})
+        .then(response => {
+            console.log(response)
+            listItems()
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
+
     return (
             <TableContainer>
                 <Table aria-label="simple table" style={{minWidth:650}}>
@@ -49,6 +65,7 @@ function InventoryTable() {
                             <TableCell align="left"><b>Description</b></TableCell>
                             <TableCell align="right"><b>Size</b></TableCell>
                             <TableCell align="left"><b>Condition</b></TableCell>
+                            <TableCell align="left"><b>Status</b></TableCell>
                             <TableCell align="center"><b></b></TableCell>
                         </TableRow>
                     </TableHead>
@@ -59,7 +76,11 @@ function InventoryTable() {
                                             <TableCell align="left"> {item.description} </TableCell>
                                             <TableCell align="right"> {item.size} </TableCell>
                                             <TableCell align="left"> {item.condition} </TableCell>
-
+                                            <TableCell align="left"> 
+                                                <IconButton size="small" onClick={() => handleSoldClick(item._id)}>
+                                                    <AttachMoneyIcon/>{item.sold ? "sold" : "unsold" }
+                                                </IconButton>
+                                            </TableCell>
                                             <TableCell align="center"> 
                                                 <IconButton size="small"><EditIcon /></IconButton> 
                                                 <IconButton size="small" onClick={() => handleClickOpen(item._id)}><DeleteIcon /></IconButton>
@@ -70,7 +91,7 @@ function InventoryTable() {
                         }
                     </TableBody>
                 </Table>
-
+                            
                 <InventoryDelete 
                     open={open}
                     handleClose={handleClose}
