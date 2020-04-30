8d746b1fadb1ee6c05861a7d00d00637f209c85d
Tim Park <timpark0807@gmail.com>
2020-04-29 20:29:22 -0700

    Home page for users that aren't logged in

---
+++ src/components/Misc/Home.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = (theme) => ({
+  paper: {
+    maxWidth: 936,
+    margin: 'auto',
+    overflow: 'hidden',
+  },
+  searchBar: {
+    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+  },
+  searchInput: {
+    fontSize: theme.typography.fontSize,
+  },
+  block: {
+    display: 'block',
+  },
+  addUser: {
+    marginRight: theme.spacing(1),
+  },
+  contentWrapper: {
+    margin: '40px 16px',
+  },
+});
+
+
+function Home(props) {
+
+  const { classes } = props;
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+
+    <Paper className={classes.paper}>
+      <AppBar className={classes.searchBar} position="static" color="default" elevation={0}>
+        <Toolbar>
+          <Grid container spacing={2} alignItems="center">
+     
+            <Grid item xs>
+              <Typography color="textSecondary" >
+                Welcome
+              </Typography>
+            </Grid>
+          
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.contentWrapper}>
+        <Typography color="textPrimary" align="center">
+        Welcome to SneakrFlip! 
+        </Typography>
+        <Typography color="textPrimary" align="center">
+        Log in to view your inventory.
+        </Typography>
+      </div>
+    </Paper>
+  );
+}
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Home);
